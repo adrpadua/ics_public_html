2ff1f2d5537ea927388a79b4b1ce629edbabcd99
adrpadua <adrpadua@gmail.com>
2017-11-11 22:57:44 -0800

    Software page Working!

---
+++ software/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.link1').hover(function () {
+        $('.link1 a').css('text-decoration', 'underline');
+    }, function() {
+        $('.link1 a').css('text-decoration', 'none');
+    })
+
+    $('.link2').hover(function () {
+        $('.link2 a').css('text-decoration', 'underline');
+    }, function() {
+        $('.link2 a').css('text-decoration', 'none');
+    })
+
+    $('.link3').hover(function () {
+        $('.link3 a').css('text-decoration', 'underline');
+    }, function() {
+        $('.link3 a').css('text-decoration', 'none');
+    })
+
+});
